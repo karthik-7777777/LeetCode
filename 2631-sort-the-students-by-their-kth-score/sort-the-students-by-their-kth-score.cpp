97047714c9dd80cd52b75abe2b93e10165dc31a4
karthik-7777777 <157501524+karthik-7777777@users.noreply.github.com>
2026-01-05 20:53:21 +0530

    Time: 7 ms (14.77%) | Memory: 32.2 MB (40.45%) - LeetSync

---
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+        sort(score.begin(),score.end(),[k](auto &a,auto &b){
+            return a[k]>b[k];
+        });
+        return score;
+    }
+};
